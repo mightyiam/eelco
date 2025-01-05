4e5c9a629fffa13604a7d3e601a3bfa23be99838
warren2k <846021+warren2k@users.noreply.github.com>
2025-01-05 10:01:50 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests/file.rs

--- tests/file.rs
+++ tests/file.rs
@@ -8,10 +8,14 @@ use util::with_eelco;
 #[test]
 fn file_creation() {
     with_eelco(|file, eelco| {
-        file.write_str(indoc! {
-        "
-                
+        file.write_str(indoc! {"
+            ```file default.nix
+            
+            ```
         "})
             .unwrap();
+
+        let file_path = file.path().to_str().unwrap();
+
     })
 }
